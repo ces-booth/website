f40444d7dd7b930105bf2fb5cf7b62e3ea8ce1fd
sarakopunova-stack <sara.kopunova@gmail.com>
2026-09-09 13:54:34 -0500

    Delete ces-website-mockup directory

--- ces-website-mockup/js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var toggle = document.querySelector(".nav-toggle");
-  if (!toggle) return;
-  toggle.addEventListener("click", function () {
-    document.body.classList.toggle("nav-open");
-  });
-});
